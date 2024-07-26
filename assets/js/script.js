66320bc53b192d3f48158a98090c40beac58f3ae
AlexandruBlbn <162574101+AlexandruBlbn@users.noreply.github.com>
2024-07-27 00:46:18 +0300

    Initial commit
    Adding files

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+function runTypingEffect() {
+  const text = 'I am Balaban Alexandru.';
+  const typingElement = document.getElementById('typing-text');
+  const typingDelay = 100;
+
+  typeText(text, typingElement, typingDelay);
+}
+
+function typeText(text, typingElement, delay) {
+  for (let i = 0; i < text.length; i++) {
+    setTimeout(() => {
+      typingElement.textContent += text.charAt(i);
+    }, delay * i);
+  }
+}
+
+function userScroll() {
+  const navbar = document.querySelector('.navbar');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+      navbar.classList.add('bg-dark');
+      navbar.classList.add('navbar-sticky');
+    } else {
+      navbar.classList.remove('bg-dark');
+      navbar.classList.remove('navbar-sticky');
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', userScroll);
+document.addEventListener('DOMContentLoaded', runTypingEffect);
